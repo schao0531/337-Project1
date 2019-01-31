59c9d80f866f55582a98ee6908ded90947802c78
Nico (School Computer) <ntyjeski@gmail.com>
2019-01-31 16:51:11 -0600

    added json parser

---
+++ json_to_pd.py
@@ -0,0 +1,20 @@
+def json_to_pd(data):
+  with open('gg2013.json') as f:
+    data = json.load(f)
+    
+  Id = []
+  text = []
+  timestamp = []
+  user_id = []
+  screen_name = []
+
+  for row in data:
+    Id.append(row['id'])
+    text.append(row['text'])
+    timestamp.append(row['timestamp_ms'])
+    user_id.append(row['user']['id'])
+    screen_name.append(row['user']['screen_name'])
+
+  table = pd.DataFrame({'text':text,'timestamp':timestamp,'user_id':user_id,'user_screen_name':screen_name},index=Id)
+  table.index.rename('id',inplace=True)
+  return table
