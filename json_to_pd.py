68d751f2f1b2e555c5d03054e9773752ab8c8b07
schao0531 <staceychao2019@u.northwestern.edu>
2019-02-09 13:19:25 -0600

    added get_hosts and get_awards

--- json_to_pd.py
+++ json_to_pd.py
@@ -1,20 +1,22 @@
+import pandas as pd
+
 def json_to_pd(data):
-  with open('gg2013.json') as f:
-    data = json.load(f)
-    
-  Id = []
-  text = []
-  timestamp = []
-  user_id = []
-  screen_name = []
+    with open('gg2013.json') as f:
+        data = json.load(f)
 
-  for row in data:
-    Id.append(row['id'])
-    text.append(row['text'])
-    timestamp.append(row['timestamp_ms'])
-    user_id.append(row['user']['id'])
-    screen_name.append(row['user']['screen_name'])
+    Id = []
+    text = []
+    timestamp = []
+    user_id = []
+    screen_name = []
 
-  table = pd.DataFrame({'text':text,'timestamp':timestamp,'user_id':user_id,'user_screen_name':screen_name},index=Id)
-  table.index.rename('id',inplace=True)
-  return table
+    for row in data:
+        Id.append(row['id'])
+        text.append(row['text'])
+        timestamp.append(row['timestamp_ms'])
+        user_id.append(row['user']['id'])
+        screen_name.append(row['user']['screen_name'])
+
+    table = pd.DataFrame({'text':text,'timestamp':timestamp,'user_id':user_id,'user_screen_name':screen_name},index=Id)
+    table.index.rename('id',inplace=True)
+    return table
